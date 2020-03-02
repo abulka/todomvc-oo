b8d543811b8f0f666cb84237d92a65aef69ad376
Andy Bulka <abulka@gmail.com>
2020-03-02 21:25:35 +1100

    initial models and controller updating via timer

--- js/app.js
+++ js/app.js
@@ -5,8 +5,31 @@
 
 	// Your starting point. Enjoy the ride!
 
-	let app = new App(new Todos())
-	console.log(app)
+	let app = new App()
 
+	// Add a couple of todo items
+	app.add("title1", "id1", false)
+	app.add("title2", "id2", false)
+
+	// Wire controllers / mediators
+	let controllers = []
+	for (let todo_item of app.todos) {
+		let controller = new ControllerTodoItem(todo_item, undefined)  // gui is undefined
+		controllers.push(controller)
+
+		// Observer Wiring
+		document.addEventListener("modified todoitem", (event) => { controller.notify(event) })
+	}
+
+
+	app.dirty_all()
+	console.log("--------------- init complete ----------")
+
+	// test update an existing item, as an experiment
+	setTimeout(function(){ 
+		app.todos[0].title += " updated"  // will auto trigger a notify
+		// let data = {id: app.todos[0].id}  // by sending data recipients can filter and be more efficient
+		// notify_all("modified todoitem", this, data)
+	 }, 2000);
 
 })(window);
--- js/app_oo.js
+++ js/app_oo.js
@@ -9,8 +9,8 @@ const util = new Util();
 class TodoItem {
 	constructor(title, id, completed) {
 		this._title = title == undefined ? "" : title;
-		this._id = id == undefined ? util.uuid() : id;
 		this._completed = completed == undefined ? false : completed;
+		this.id = id == undefined ? util.uuid() : id;  // no getter/setter needed
 	}
 
 	get title() {
@@ -19,13 +19,22 @@ class TodoItem {
 
 	set title(v) {
 		this._title = v;
-		notify_all("modified title", this, this._title);
+		notify_all("modified todoitem", this);
+	}
+
+	get completed() {
+		return this._completed;
+	}
+
+	set completed(v) {
+		this._completed = v;
+		notify_all("modified todoitem", this);
 	}
 }
 
-class Todos {
+class App {
 	constructor(todos) {
-		this._todos = todos == undefined ? [] : todos;
+		this._todos = todos == undefined ? [] : todos;  // existing todos from persistence
 	}
 
 	get todos() {
@@ -37,11 +46,66 @@ class Todos {
 		this._todos.push(todo);
 		// probably need to notify
 	}
+
+    dirty_all() {
+		// notify_all("modified todoitem", this)
+		notify_all("modified todoitem", this)
+	  }	
 }
 
-class App {
-	// aggregates all the sub models into one housing, with some business logic
-	constructor(todos_model) {
-		this.todos_model = todos_model
+
+// Controllers / Mediators
+
+class ControllerTodoItem {
+	constructor(model_ref, gui_id) {
+	  this.model_ref = model_ref
+	  this.gui_id = gui_id  // dom element has id matching model id, could be done in other ways too
+	  this.parent_gui_ref = 'ul.todo-list'
 	}
-}
+  
+	// need keychar ENTER to trigger this change
+	on_keychar_welcome(e) { this.welcome_model.message = $(e.target).val() }
+  
+	notify(event) {  
+		// sender's notify_all(event_name, from, data) - the 'from' and 'data' end up in event.detail dict
+
+		if (this.gui_id == undefined) {
+			// Gui element has not been created yet, so build it and inject it
+			console.log(`controller for ${this.model_ref.title} got notified to build initial gui`)
+
+			this.gui_id = this.model_ref.id  // use id of model for the gui id, can't think of a better gui ref technique
+			let li = `<li id="${this.gui_id}">${this.model_ref.title}</li>`
+			/*
+			TODO - NEED TO INSERT A PROPER TEMPLATED ITEM
+	
+			// $('.todo-list').html(this.todoTemplate(todos));
+	
+			<script id="todo-template" type="text/x-handlebars-template">
+				{{#this}}
+				<li {{#if completed}}class="completed"{{/if}} data-id="{{id}}">
+					<div class="view">
+						<input class="toggle" type="checkbox" {{#if completed}}checked{{/if}}>
+						<label>{{title}}</label>
+						<button class="destroy"></button>
+					</div>
+					<input class="edit" value="{{title}}">
+				</li>
+			{{/this}}
+			</script>
+			*/
+	
+			let $last_item = $(`${this.parent_gui_ref} li`).last()
+			$(li).insertAfter($last_item)
+			}
+		else {
+			if (this.model_ref.id == event.detail.from.id) {
+				// Gui element already exists, simply update it
+				console.log(`controller for ${this.model_ref.title} got notified with detail ${JSON.stringify(event.detail)}`)
+				let $li_el = $(`#${this.gui_id}`)
+				$li_el.text(this.model_ref.title)  // TODO need to make this the label inside the template
+			}
+		}
+
+	}
+  }
+
